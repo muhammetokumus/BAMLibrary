83af9b06d8e30b15a52c0cc34f1ef3fe4b5c232d
Muhammet <okumusmuhammed15@gmail.com>
2022-12-08 01:33:31 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/BAMLibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using BAM.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAM.DataAccess.Concrete.EntityFramework
+{
+    public class BAMLibraryContext : IdentityDbContext<AppUser,IdentityRole<int>,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;DataBase=BAMLibrary;Trusted_connection=true;");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DataAccess/Migrations/20221201162355_mig_2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BAM.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Star",
+                table: "Books");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Books");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Star",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
